60c442cae2580af2c844862678f4210c3735b8b3
Armour <1102449269@qq.com>
2017-04-18 22:11:22 +0800

    css/js

--- first.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by pengxiang on 04/07 0007.
- */
-
-function a() {
-    var message="sss";
-    alert(message);
-}
-a();
---
+++ js/main.js
@@ -0,0 +1,39 @@
+/**
+ * Created by pengxiang on 04/07 0007.
+ */
+
+;$(function () {
+
+    var sidebar = $('#side-bar'),//xuanze
+        mask = $('.mask'),
+        backButton = $('#back-to-top'),
+        sidebar_trigger = $('#sidebar_trigger');
+
+    function showSideBar() {
+        mask.fadeIn();
+        sidebar.animate({'right':0});
+    }
+    function hideSideBar() {
+        mask.fadeOut();
+        sidebar.animate({'right':-sidebar.width()});
+    }
+    function backToTop() {
+        $('html,body').animate({
+            scrollTop: 0
+        },600)
+    }
+
+    sidebar_trigger.on('click',showSideBar);
+    mask.on('click',hideSideBar);
+    backButton.on('click',backToTop)
+
+    $(window).on('scroll',function (){
+        if ($(window).scrollTop() > $(window).height())
+            backButton.fadeIn();
+        else
+            backButton.fadeOut();
+    })
+
+    $(window).trigger('scroll' );
+})
+
